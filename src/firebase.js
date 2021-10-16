b197f03260df752b7633f2b06373519b23d265a9
ly son trung <trungls1706@gmail.com>
2021-10-17 01:44:08 +0700

    init header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-zRuYLxUSk38pweXwUh1V_x7Uj0ivYq8",
+    authDomain: "e-clone-3d207.firebaseapp.com",
+    projectId: "e-clone-3d207",
+    storageBucket: "e-clone-3d207.appspot.com",
+    messagingSenderId: "926699295079",
+    appId: "1:926699295079:web:3dbc2b5f6f67e37d3a5fc2",
+    measurementId: "G-ZRFTBG7RJY"
+};
+
